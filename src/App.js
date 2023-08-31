38089b717b5540fa497868e8de013e02999294a0
huy.tran@codecomplete.jp <Nhathuy2401>
2023-08-31 11:10:28 +0700

    revert url

--- src/App.js
+++ src/App.js
@@ -2,16 +2,7 @@ import { useEffect } from 'react';
 
 export default function Home() {
   useEffect(() => {
-    if(window.location.host.includes('facebook.com' || 'fbclid')) {
-      window.location.href = 'https://nailsforus.com';
-    }
-
-    // if (window.location.hostname !== 'localhost') {
-      
-    // }
-
-
-    // window.location.href = 'https://nailsforus.com';
+    window.location.href = 'https://nailsforus.com';
   }, []);
 
   return null;
